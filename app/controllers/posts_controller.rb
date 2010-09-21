d450e12fb635a156e2e75b317281eeac28185447
Tracy LOISEL <tracy.loisel@onwild.com>
2010-09-21 02:48:51 +0200

    Optimize posts/index sql requests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.xml
   def index
-    @posts = Post.find(:all, :order => "position, created_at DESC")
+    @posts = Post.find(:all, :order => "position, created_at DESC", :include => [:assets, :comments])
 
     respond_to do |format|
       format.html # index.html.erb
